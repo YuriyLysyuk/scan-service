727ea65f285a0347ef71aea591f51f820b87c901
YuriyLysyuk <lysyuk.y@yandex.ru>
2023-01-17 10:42:18 +0300

    add initial main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import UIPage from './pages/UIPage/UIPage';
+import MainPage from './pages/MainPage/MainPage';
 
 const App = () => {
-  return <UIPage />;
+  return <MainPage />;
 };
 
 export default App;
---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import MainLayout from '../../layouts/MainLayout/MainLayout';
+
+const MainPage = () => {
+  return (
+    <MainLayout>
+    </MainLayout>
+  );
+};
+
+export default MainPage;
