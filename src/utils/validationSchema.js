531b3019b651c73c82954353d2bab2704a274022
YuriyLysyuk <lysyuk.y@yandex.ru>
2023-01-25 14:21:30 +0300

    expand phone validation rule

--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -1,7 +1,12 @@
 import * as Yup from 'yup';
 
 const validationSchema = Yup.object({
-  phone: Yup.string().required('Введите логин или номер телефона'),
+  phone: Yup.string()
+    .matches(
+      /^\+7 [\d]{3} [\d]{3} [\d]{2} [\d]{2}$/,
+      'Введите корректный номер телефона'
+    )
+    .required('Введите номер телефона'),
   password: Yup.string().required('Введите пароль'),
 });
 
