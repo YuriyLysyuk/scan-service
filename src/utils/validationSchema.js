bc2ed0819ada57c26482f92404906cb8e912051c
YuriyLysyuk <lysyuk.y@yandex.ru>
2023-01-30 11:42:14 +0300

    comment phone validation scheme

--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -1,14 +1,14 @@
 import * as Yup from 'yup';
 
 const validationSchema = Yup.object({
-  phone: Yup.string()
-    .matches(
-      /^\+7 [\d]{3} [\d]{3} [\d]{2} [\d]{2}$/,
-      'Введите корректный номер телефона'
-    )
-    .required('Введите номер телефона'),
   login: Yup.string().required('Введите логин'),
   password: Yup.string().required('Введите пароль'),
+  // phone: Yup.string()
+  //   .matches(
+  //     /^\+7 [\d]{3} [\d]{3} [\d]{2} [\d]{2}$/,
+  //     'Введите корректный номер телефона'
+  //   )
+  //   .required('Введите номер телефона'),
 });
 
 export default validationSchema;
