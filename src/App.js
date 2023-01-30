14f211ab7f5c1bbdcd24b82e52a934c65db49745
YuriyLysyuk <lysyuk.y@yandex.ru>
2023-01-30 17:13:47 +0300

    save auth after reload page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import { HOME_URL, LOGIN_URL } from './constants';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
 import MainPage from './pages/MainPage/MainPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route index path={HOME_URL} element={<MainPage />} />
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route index path={HOME_URL} element={<MainPage />} />
 
-          <Route path={LOGIN_URL} element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
+            <Route path={LOGIN_URL} element={<LoginPage />} />
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { scanApi } from './api/scan';
 import authReducer from './slices/authSlice';
 
+const persistConfig = {
+  key: 'auth',
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
 export const store = configureStore({
   reducer: {
     [scanApi.reducerPath]: scanApi.reducer,
-    auth: authReducer,
+    auth: persistedAuthReducer,
   },
-
-  devTools: process.env.NODE_ENV === 'development',
 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(scanApi.middleware),
 });
+
+export const persistor = persistStore(store);
