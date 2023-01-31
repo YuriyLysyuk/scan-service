b4b3ff6a2d95401c683b55d4449a4e371c708a78
YuriyLysyuk <lysyuk.y@yandex.ru>
2023-01-31 12:38:43 +0300

    refactor index and app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
+import { persistor, store } from './redux/store';
 import './index.scss';
 import App from './App';
 
@@ -8,6 +12,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
