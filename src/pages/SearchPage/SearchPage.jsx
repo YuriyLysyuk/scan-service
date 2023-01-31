863f35db081f945055ea5c7d468ac5f8ed645adf
YuriyLysyuk <lysyuk.y@yandex.ru>
2023-01-31 17:06:56 +0300

    add search page markup and styles

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -3,14 +3,27 @@ import React from 'react';
 import styles from './styles.module.scss';
 
 import MainLayout from '../../layouts/MainLayout/MainLayout';
+import SearchForm from '../../components/SearchForm/SearchForm';
+import { ReactComponent as ManWithRocketSVG } from '../../assets/images/man-with-rocket.svg';
 
 const SearchPage = () => {
   return (
     <MainLayout>
       <section className={styles.search}>
-        <h1 className={styles.search__title}>
-          Найдите необходимые данные в пару кликов.
-        </h1>
+        <div className={styles.search__text}>
+          <h1 className={styles.search__title}>
+            Найдите необходимые данные в пару кликов.
+          </h1>
+
+          <p className={styles.search__desc}>
+            Задайте параметры поиска. <br />
+            Чем больше заполните, тем точнее поиск
+          </p>
+        </div>
+
+        <SearchForm className={styles.search__form} />
+
+        <ManWithRocketSVG className={styles.search__img} />
       </section>
     </MainLayout>
   );
