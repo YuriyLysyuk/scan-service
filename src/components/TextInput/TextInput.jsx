29515820ffdd7802ff3e176ec302d83c042f1650
YuriyLysyuk <lysyuk.y@yandex.ru>
2023-02-01 13:51:39 +0300

    exclude wrap from TextInput

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -1,31 +1,27 @@
 import React from 'react';
 import { useField } from 'formik';
-import classNames from 'classnames';
 import MaskedInput from 'react-text-mask';
 
 import styles from './styles.module.scss';
 import getMaskByInputName from './masks';
 
+import InputWrap from '../InputWrap/InputWrap';
+
 const TextInput = ({ label, className, ...props }) => {
   const [field, meta] = useField(props);
-  const hasError = meta.touched && meta.error;
 
-  const groupClass = classNames(styles.group, className, {
-    [`${styles.error}`]: hasError,
-  });
+  const wrapOptions = {
+    label,
+    field,
+    meta,
+  };
 
   const mask = getMaskByInputName(field);
 
   return (
-    <div className={groupClass}>
-      <label className={styles.label} htmlFor={props.id || props.name}>
-        {label}
-      </label>
-
+    <InputWrap className={className} options={wrapOptions}>
       <MaskedInput className={styles.input} {...field} {...props} mask={mask} />
-
-      {hasError ? <div className={styles.message}>{meta.error}</div> : null}
-    </div>
+    </InputWrap>
   );
 };
 
