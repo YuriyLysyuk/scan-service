0932093acf641250872dbc2561b9d7900dc54ed6
YuriyLysyuk <lysyuk.y@yandex.ru>
2023-02-02 13:55:54 +0300

    add DatePickerInput

---
+++ src/components/DatePickerInput/DatePickerInput.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useField, useFormikContext } from 'formik';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+import styles from './styles.module.scss';
+
+const DatePickerInput = ({ placeholder, ...props }) => {
+  const { setFieldValue } = useFormikContext();
+
+  const date = (props.value && new Date(props.value)) || null;
+
+  const onDateChange = (val) => {
+    setFieldValue(props.name, val);
+  };
+
+  return (
+    <DatePicker
+      className={styles.input}
+      {...props}
+      dateFormat="dd.MM.yyyy"
+      selected={date}
+      onChange={onDateChange}
+      placeholderText={placeholder}
+    />
+  );
+};
+
+export default DatePickerInput;
