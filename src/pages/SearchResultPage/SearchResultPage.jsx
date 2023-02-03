2d5cb6e96a1e64319fe5beebc00ff638aafbbc6b
YuriyLysyuk <lysyuk.y@yandex.ru>
2023-02-03 19:27:12 +0300

    add button to go to search page if no search data

--- src/pages/SearchResultPage/SearchResultPage.jsx
+++ src/pages/SearchResultPage/SearchResultPage.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { useActionData } from 'react-router-dom';
+import { useActionData, useNavigate } from 'react-router-dom';
 
 import styles from './styles.module.scss';
 
+import { SEARCH_URL } from '../../constants';
 import MainLayout from '../../layouts/MainLayout/MainLayout';
+import Button from '../../components/Button/Button';
 import { ReactComponent as WomanWithLupaSVG } from '../../assets/images/woman-with-lupa.svg';
 
 const SearchResultPage = () => {
   document.title = 'Результаты поиска публикаций о компании по его ИНН — СКАН';
 
+  const navigate = useNavigate();
   const actionData = useActionData();
 
   console.log(actionData);
@@ -23,6 +26,16 @@ const SearchResultPage = () => {
             Поиск может занять некоторое время, <br />
             просим сохранять терпение
           </p>
+
+          {!Boolean(actionData) && (
+            <Button
+              className={styles.hero__btn}
+              color="primary"
+              onClick={() => navigate(SEARCH_URL)}
+            >
+              Задать параметры поиска
+            </Button>
+          )}
         </div>
 
         <WomanWithLupaSVG className={styles.hero__img} />
