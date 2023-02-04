1eb190e8d54b1a52511874158407c11ba20f4dab
YuriyLysyuk <lysyuk.y@yandex.ru>
2023-02-04 15:11:42 +0300

    add reduceHistogramsByDate helper

--- src/redux/api/helpers.js
+++ src/redux/api/helpers.js
@@ -4,3 +4,23 @@ export const getObjectSearchTotal = (totalDocuments) => {
   return totalDocuments.reduce((sum, { value }) => (sum += value), 0);
 };
 
+export const reduceHistogramsByDate = (data) => {
+  const totalDocuments = data.data[0];
+  const riskFactors = data.data[1];
+
+  const result = totalDocuments.data
+    .reduce((data, { date, value }, i) => {
+      const item = {
+        date: Date.parse(date),
+        totalDocuments: value,
+        riskFactors: riskFactors.data[i].value,
+      };
+
+      data.push(item);
+
+      return data;
+    }, [])
+    .sort(({ dateA }, { dateB }) => dateA - dateB);
+
+  return result;
+};
